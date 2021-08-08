31ea1b504f147775a217cc98540ae301436d1ae7
Shariar13 <88611911+Shariar13@users.noreply.github.com>
2021-08-09 00:25:54 +0600

    Update urls.py

--- helpme/urls.py
+++ helpme/urls.py
@@ -6,6 +6,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +16,4 @@ urlpatterns = [
     path('profile/',include('post.urls')),
     path('chat/', include('chat.urls')),
     
-] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
